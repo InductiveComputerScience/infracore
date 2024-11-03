5d2b1496e9aa3a47f3faf9a353c1508405890fdf
Martin F. Johansen <martinfjohansen@inductive.no>
2024-11-03 15:35:47 +0100

    audio demo in assembly

--- examples/audio-demo2/program.asm
+++ examples/audio-demo2/program.asm
@@ -2,17 +2,20 @@
 
 section .bss                              ; Global variables initialized to 0
 
-samples: resq 48000*2                     ; The image to be displayed, color bytes: R, G, B, A
-size: resq 1
-audio: resq 1                             ; The pointer to the screen device
-counter: resq 1                           ; Loop counter variable
-samplesps: resq 1
-hz: resq 1
-cur: resq 1
-delay: resq 1
-mindelay: resq 1
-x: resq 1
-samplesWritten: resq 1
+  samples: resq 48000*2                   ; The image to be displayed, color bytes: R, G, B, A
+  size: resq 1                            ; The size of the audio buffer
+  mindelay: resq 1                        ; The minimum delay of the audio
+  audio: resq 1                           ; The pointer to the screen device
+  counter: resq 1                         ; Loop counter variable
+  samplesps: resq 1                       ; Samples per second for the audio device
+  cur: resq 1                             ; Current position of audio samples sent to audio device
+  delay: resq 1                           ; The current delay of the audio
+  tmp: resq 1                             ; Temporary variable used for x87 loads
+  samplesWritten: resq 1
+
+section .rodata                           ; Read-only data
+
+  pi: dq __?float64?__(3.14159265358979)  ; π
 
 section .text                             ; Program data
 
@@ -20,92 +23,81 @@ global Program                            ; Make "Program" visible outside
 Program:                                  ; Start of Program
   enter 0, 0                              ; Start new stack frame
   mov [audio], rdi                        ; Store audio pointer in variable
-  mov qword [samplesps], 48000
-	mov qword [size], 48000*2
-  mov rax, __?float64?__(261.626)         ; C tone hertz
-	mov qword [hz], rax
-	mov qword [mindelay], 9600
 
-	finit
+  ; Get the specs of the audio device
+  mov rdi, [audio]                        ; (audio, &samplesps, &mindelay)
+  mov rsi, samplesps                      ; ---
+  mov rdx, mindelay                       ; ---
+  call AudioSamplesMinDelay               ; Get samples per second and minimum delay
 
-	; Create pure C tone sine wave
-  mov qword [counter], 0                  ; Loop this many times
+  ; Compute the size of the audio sample
+  cvtsd2si rax, [samplesps]
+  imul rax, 2                             ; 2 seconds of audio
+  mov [size], rax
+
+  finit                                   ; Initialize the x87 floating point unit
+
+  ; Create pure C tone sine wave
+  mov qword [counter], 0                  ; Start at counter = 0
   loop:                                   ; Start of loop
-		fild qword [counter]                  ; Expression: sin(i/samplesps*(2*M_PI) * hz)
-		fild qword [samplesps]
-		fdiv
-    fldpi
-    fldpi
-		fadd
-		fmul
-		fld qword [hz]
-		fmul
-		fsin
-		mov rax, samples
-		mov rcx, [counter]
-		fstp qword [rax + rcx*8]
+    cvtsi2sd xmm0, [counter]              ; expression: sin(counter/samplesps*(2*pi) * hz)
+    divsd xmm0, [samplesps]               ; counter/samplesps
+    mov rax, 2                            ; 2*pi
+    cvtsi2sd xmm1, rax                    ; ---
+    mulsd xmm1, [pi]                      ; ---
+    mulsd xmm0, xmm1                      ; counter/samplesps*(2*pi)
+    mov rax, __?float64?__(261.626)       ; middle C tone hertz
+    movq xmm2, rax                        ; 
+    mulsd xmm0, xmm2                      ; counter/samplesps*(2*pi) * hz
+    movq [tmp], xmm0                      ; sin(counter/samplesps*(2*pi) * hz)
+    fld qword [tmp]                       ; ---
+    fsin                                  ; ---
+    mov rax, samples                      ; Write sample
+    mov rcx, [counter]                    ; ---
+    fstp qword [rax + rcx*8]              ; ---
 
-    inc qword [counter]                   ; Decrement loop variable
-    cmp qword [counter], 48000*2
-  jl loop                                 ; If zero, quit loop
+    inc qword [counter]                   ; Increment loop variable
+    mov rax, [size]                       ; while counter < size
+    cmp [counter], rax                    ; ---
+  jl loop                                 ; ---
 
-	; Play sound
-	mov qword [cur], 0
-	playloop:
-		mov rdi, [audio]                      ; parameters: (audio)
-		call CurrentDelay                     ; Get the current delay
-		cvtsd2si rax, xmm0                    ; Get the return value
-		mov qword [delay], rax
+  ; Play sound
+  mov qword [cur], 0                      ; Play audio from the start
+  playloop:
+    ; Get the current audio delay
+    mov rdi, [audio]                      ; parameters: (audio)
+    call CurrentDelay                     ; Get the current delay
+    movq [delay], xmm0                    ; Get the return value
 
-		; if(delay < mindelay * 1.5){         ; delay < mindelay * 1.5
-		finit
-		fild qword [mindelay]
-		mov rax, __?float64?__(1.5)           ;
-		mov [x], rax
-		fld qword [x]
-		fmul
-		fild qword [delay]                    ; 
-		fcomi
+    movq xmm0, [delay]                    ; if delay < mindelay * 1.5 
+    movq xmm1, [mindelay]                 ; ---
+    mov rax, __?float64?__(1.5)           ; t1 = 1.5
+    cvtsi2sd xmm2, rax                    ; ---
+    mulsd xmm1, xmm2                      ; t2 = mindelay * t1
+    comisd xmm0, xmm1                     ; delay < t2 -- set EFLAGS accordingly
 
-		ja skip
-			mov rdi, [audio]                    ; (audio, samples + cur, fmin(mindelay, size - cur), &samplesWritten)
-			mov rcx, qword [cur]
-			lea rsi, qword [samples + rcx*8]
-			cvtsi2sd xmm1, qword [size]
-			cvtsi2sd xmm2, qword [cur]
-			subsd xmm1, xmm2
-			cvtsi2sd xmm0, qword [mindelay]
-			minsd xmm0, xmm1
-			mov rdx, samplesWritten
-			call WriteSamples
-			cvtsd2si rax, [samplesWritten]
-			add [cur], rax
-		skip:
+    ja skip
+      ; Write more audio samples
+      mov rdi, [audio]                    ; Set up parameters: (audio, samples + cur, fmin(mindelay, size - cur), &samplesWritten)
+      mov rcx, [cur]                      ; samples + cur
+      lea rsi, [samples + rcx*8]          ; ---
+      cvtsi2sd xmm1, [size]               ; fmin(mindelay, size - cur)
+      cvtsi2sd xmm2, [cur]                ; ---
+      subsd xmm1, xmm2                    ; ---
+      movq xmm0, [mindelay]               ; ---
+      minsd xmm0, xmm1                    ; ---
+      mov rdx, samplesWritten             ; &samplesWritten
+      call WriteSamples                   ; Write more audio samples
+      cvtsd2si rax, [samplesWritten]      ; cur += samplesWritten
+      add [cur], rax                      ; ---
+    skip:
 
-		pause                                  ; Tell CPU it is a spin-wait
+    pause                                 ; Tell CPU this is a spin-wait
 
-		mov rax, qword [cur]
-		cmp rax, qword [size]
-	jl playloop
+    mov rax, [cur]                        ; while cur < size
+    cmp rax, [size]                       ; ---
+  jl playloop                             ; ---
 
-  leave                                    ; Done with function
-  ret                                      ; Return
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  leave                                   ; Done with function
+  ret                                     ; Return
 
