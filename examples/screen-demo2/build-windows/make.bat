42081987789321ac3b2d32c619f1201b5ffe0659
Martin F. Johansen <martinfjohansen@inductive.no>
2024-11-05 19:49:22 +0100

    windows make file

---
+++ examples/screen-demo2/build-windows/make.bat
@@ -0,0 +1,12 @@
+REM Program 1
+REM - Devices
+cl /c ../../../drivers/screen-windows-winapi/screen-windows-winapi.c /I ../../../devices/screen/
+
+REM - Infrastructural Program
+cl /c launcher.c /I ../../../devices/screen
+
+REM Program
+cl /c ../program.c /I ../../../devices/screen
+
+REM - Combine the whole program
+link /out:program.exe program.obj launcher.obj screen-windows-winapi.obj user32.lib gdi32.lib
