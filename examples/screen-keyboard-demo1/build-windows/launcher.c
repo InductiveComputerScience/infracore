ff348b9469ab6b429acedccd8b523450a27e458d
Martin F. Johansen <martinfjohansen@inductive.no>
2024-11-12 14:14:14 +0100

    windows build for screen keyboard demo

---
+++ examples/screen-keyboard-demo1/build-windows/launcher.c
@@ -0,0 +1,43 @@
+#include <Screen.h>
+#include <Keyboard.h>
+
+#include "../../../drivers/screen-windows-winapi/screen-windows-winapi.h"
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <math.h>
+
+#include "../program.h"
+
+int WINAPI WinMain(HINSTANCE rInstance, HINSTANCE PrevInstance, LPSTR CmdLine, int CmdShow) {
+  int success;
+  int returnCode;
+  ScreenStructure *screen1;
+  KeyboardStructure *keyboard1;
+
+  success = CreateScreenWinAPI(&screen1, rInstance, 640, 480, 1920/0.508);
+
+  if(success){
+    success = CreateKeyboardWinAPI(&keyboard1, screen1);
+  }
+
+  if(success){
+    Program(screen1, keyboard1);
+
+    CloseScreenWinAPI(screen1);
+    CloseKeyboardWinAPI(keyboard1);
+  }
+
+  if(success){
+    returnCode = 0;
+  }else{
+    returnCode = 1;
+  }
+
+  return returnCode;
+}
+
+
+
